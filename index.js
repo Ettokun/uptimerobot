31ceeeff356a2b48cbf500af5268b2cc558defb8
Ettokun <48173337+Ettokun@users.noreply.github.com>
2020-07-22 13:41:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://crawlbeed.glitch.me", "https://amelia-official.glitch.me"]
+const urls = ["https://crawlbeed.glitch.me", "https://amelia-official.glitch.me", ""https://teitokuofficial.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Terping ke glitch kontol! udh online." + Date.now())).catch(() => {});
